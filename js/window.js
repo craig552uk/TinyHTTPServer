69cbaab52d048eca82f3e4ec72a9274408b2d031
Craig Russell <craig@craig-russell.co.uk>
2015-02-22 08:42:25 +0000

    Added basic window styles and functionality

--- js/window.js
+++ js/window.js
@@ -3,6 +3,18 @@
  * Scripts for the app UI
  */
 
-(function(){
+(function(window, $){
 
-})
+  var output = $('#output');
+
+  var print_msg = function(message){
+    output.append('<p>' + message + '</p>');
+    output.scrollTop(output.height());
+  }
+
+  output.click(function(){
+    print_msg("Sample Output at " + new Date());
+  });
+
+
+})(window, jQuery)
